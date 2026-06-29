e92f72563016ccbd2479da8b421a2678e5b9b46e
Gabriel Santos <gabrielborbasts@gmail.com>
2026-06-29 17:46:55 -0300

    Documentação jogadores

--- src/model/jogador/JogadorHumano.java
+++ src/model/jogador/JogadorHumano.java
@@ -1,12 +1,11 @@
 package model.jogador;
 
-import java.util.InputMismatchException;
 import model.DAO.RegistroJogador;
 import model.carta.Carta;
-import model.jogador.Jogador;
 
 /**
- * Classe para o jogador humano que herda atributos e métodos de jogador
+ * Classe que representa o jogador controlado pela usuário.
+ * Recebe um RegistroJogador e inicializa o jogador humano.
  *
  * @author gbrsnts
  */
@@ -14,20 +13,24 @@ public class JogadorHumano extends Jogador {
     private RegistroJogador registro;
     
     public JogadorHumano(RegistroJogador registro) {
+        
+        // Define o nome do jogador na classe base
         super(registro.getName());
+        
+        // Guarda referência ao registro completo
         this.registro = registro;
     }
     
+    // Retorna o registro completo do jogador.
+    // Permitindo acessar informações persistidas como ID, nome e data de criação.
     public RegistroJogador getRegistro(){
         return registro;
     }
     
-    /**
-     * A implementação deste modo difere do jogadorMaquina, 
-     * pois o jogadorHumano precisa receber um indice para remover a carta
-     * da mão do jogador.
-     */
-    
+    /*
+    Diferente da máquina, o jogador humano não escolhe aleatoriamente.
+    Ele recebe um índice externo, vindo da interação do usuário.
+    */
     @Override
     public Carta escolherCarta(int indice) {    
         return mao.remove(indice);
--- src/model/jogador/JogadorMaquina.java
+++ src/model/jogador/JogadorMaquina.java
@@ -5,7 +5,10 @@ import model.jogador.Jogador;
 import model.carta.Carta;
 
 /**
- * Classe onde define alguns comportamentos do jogadorMaquina
+ * Classe que representa o jogador controlado pela máquina.
+ * 
+ * A classe herda de jogador e implementa o comportamento de escolher carta.
+ * 
  * @author gbrsnts
  */
 public class JogadorMaquina extends Jogador {
@@ -17,7 +20,10 @@ public class JogadorMaquina extends Jogador {
 
     @Override
     public Carta escolherCarta(int indice) {
+        // Gera um índice aleatório baseado no tamanho da mão.
         int index = random.nextInt(mao.size());
+        
+        // Remove e retorna a carta escolhida.
         return mao.remove(index);
     }
     
