ea52960792a92109a128f449b7177734dad776c2
Gabriel Santos <gabrielborbasts@gmail.com>
2026-06-29 18:01:54 -0300

    Documentação de model.Turno

--- src/model/game/Turno.java
+++ src/model/game/Turno.java
@@ -6,7 +6,7 @@ import model.jogador.JogadorHumano;
 import view.game.ViewTurno;
 
 /**
- * Classe responsável pela classe e lógica de cada turno
+ * Classe responsável pela lógica de cada turno.
  * 
  * @author gbrsnts
  */
@@ -21,29 +21,39 @@ public class Turno {
     
     public Jogador jogar(){
         Carta carta1 = jogarCarta(primeiro);    
+        
+        // Exibe na view a carta escolhida.
         ViewTurno.mostrarJogada(primeiro, carta1);        
         
         Carta carta2 = jogarCarta(segundo);
+        
+        // Exibe na view a carta escolhida.
         ViewTurno.mostrarJogada(segundo, carta2);
         
-        if (carta1.getValor() > carta2.getValor()) {
-            return primeiro;
-        }
+        // Realiza a comparação entre as cartas jogadas
+        if (carta1.getValor() > carta2.getValor()) return primeiro;
+        if (carta2.getValor() > carta1.getValor()) return segundo;
 
-        if (carta2.getValor() > carta1.getValor()) {
-            return segundo;
-        }
-
+        // Regra de desempate: caso ambas cartas tenha o mesmo valor,
+        // O primeiro a jogar a carta será o vencedor.
         return primeiro;
     }
     
+    /* Executa a jogada de cada jogador.
+    Esse método diferencia o jogador de humano e máquina. */
     private Carta jogarCarta(Jogador jogador) {
-
+        
+        // Verifica se o jogador é humano para o solicitar input ou não.
         if (jogador instanceof JogadorHumano) {
+            
+            // A view solicita que o jogador escolha uma carta.
             int indice = ViewTurno.escolherCarta(jogador);
+            
+            // Retorna a carta escolhida pelo jogador humano.
             return jogador.escolherCarta(indice);
         }
-
+        
+        // Caso seja a vez da máquina, será sempre passado o índice 0.
         return jogador.escolherCarta(0);
     }
 }
