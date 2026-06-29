1c082256c57799bdd6a652c2807f6d9b72e97b20
Gabriel Santos <gabrielborbasts@gmail.com>
2026-06-29 13:40:32 -0300

    Comentários da main

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@ import controller.MenuController;
 import view.Menu;
 
 /**
- *
+ * Classe principal para iniciar a aplicação.
+ * 
  * @author gbrsnts
  */
 public class Main {
 
     public static void main(String[] args) {
-        // Instancia e inicia o menu
+        // Instancia o controlador principal do sistema
         MenuController controller = new MenuController();
+        
+        // Instancia a interface de menu
         Menu menu = new Menu(controller);
+        
+        // Inicia a execução da aplicação
         menu.iniciar();
     }
 }
